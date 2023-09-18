daef294f28d5582bf064f902d8914d7bdee16c6c
Timpan42 <tim@fagerdal.se>
2023-09-18 08:05:39 +0200

    💬 added text

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,7 +3,9 @@ function Homepage() {
         <>
             <div className="container region flow">
                 <h1>Home</h1>
-                <p>awdawdawdawdawdawdawdawdawdawd</p>
+                <p>This website has projects that I have made.</p>
+                <p>The Todo list is a page where you can make a todo list. </p>
+                <p>The Epic Story is a page were you can play my choice based game. <b>I am converting the Javascript code to React so it is still in progress!</b></p>
             </div>
         </>
     )
